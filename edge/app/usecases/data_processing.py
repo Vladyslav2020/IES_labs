cf6c70b9aea2874d364b41d3aeed4d24934fe088
VMoskalenko <vladyslav.moskalenko@microfocus.com>
2024-03-15 12:46:06 +0200

    Lab4: implement process_agent_data method in data_processing.py

--- edge/app/usecases/data_processing.py
+++ edge/app/usecases/data_processing.py
@@ -1,9 +1,20 @@
-from app.entities.agent_data import AgentData
+from app.entities.agent_data import AgentData, AccelerometerData
 from app.entities.processed_agent_data import ProcessedAgentData
 
 
+def classify_road_state(accelerometer_data: AccelerometerData) -> str:
+    z_axis_value = accelerometer_data.z
+
+    if z_axis_value < 10000:
+        return "bumpy"
+    elif z_axis_value > 16600:
+        return "hilly"
+    else:
+        return "normal"
+
+
 def process_agent_data(
-    agent_data: AgentData,
+        agent_data: AgentData,
 ) -> ProcessedAgentData:
     """
     Process agent data and classify the state of the road surface.
@@ -12,4 +23,6 @@ def process_agent_data(
     Returns:
         processed_data_batch (ProcessedAgentData): Processed data containing the classified state of the road surface and agent data.
     """
-    # Implement it
+    road_state = classify_road_state(agent_data.accelerometer)
+    processed_data = ProcessedAgentData(road_state=road_state, agent_data=agent_data)
+    return processed_data
